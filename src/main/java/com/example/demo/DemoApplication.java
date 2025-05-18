2b4b5abf962ad3cb63b40c4d9da67ed8b80d8972
cactus-y <youseokwon1667@gmail.com>
2025-05-18 17:53:56 +0000

    added a comment

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		// This is the main method that starts the Spring Boot application
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
